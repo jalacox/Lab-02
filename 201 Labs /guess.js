091f80ca2bffc1230332f06eaa94a574770b83a6
JALA COX <JALACOX17@GMAIL.COM>
2023-02-27 10:31:38 -0600

    jala

---
+++ 201 Labs /guess.js
@@ -0,0 +1,9 @@
+
+var question = [
+    {
+        prompt: "How old am I?"\n(a) 24\n\(b) 25\n(c) 98",
+        answer: "a"
+    },
+    {
+        prompt: "What is my favorite color?"\n(a) Purple\n\
+    }
